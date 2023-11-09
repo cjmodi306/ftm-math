43961f6e2a208803a711199bea84e9a90f2bd413
Chirag Modi <55592063+cjmodi306@users.noreply.github.com>
2023-11-09 19:21:26 +0100

    Add files via upload

---
+++ a1.m
@@ -0,0 +1,29 @@
+clc;clear all;
+
+function ans=func_1(x)
+  ans = (x**6) + (3*(x**4) - (x**3) - (5*x);
+endfunction
+
+function ans=func_2(x)
+  ans = (3*(x**5)) - (4*(x**3)) + (2*x);
+endfunction
+
+function ans=func_4(x)
+  ans = sind(x) + (3*exp(-x)*cosd(x));
+endfunction
+
+function ans=func_1(a,b)
+  ans = a+b;
+endfunction
+
+function ans=func_1(a,b)
+  ans = a+b;
+endfunction
+
+function ans=func_1(a,b)
+  ans = a+b;
+endfunction
+
+a=1;
+b=2;
+fprintf("%d", sum(a,2*b));
